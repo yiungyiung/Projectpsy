d4b8ea34f728f415e004911e5f61755c2661de37
yiungyiung <yashparkhi.super@gmail.com>
2023-07-31 00:53:40 +0530

    added main project

---
+++ Assets/MessageListener.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageListener : MonoBehaviour
+{
+    public string[] data;
+    public moveleg leg;
+    void OnMessageArrived(string msg)
+    {   
+        string[] data =msg.Split(',');
+        if (data.Length>3)
+        {
+            leg.data = data;
+        }
+
+    }
+
+
+    
+    void OnConnectionEvent(bool success)
+    {
+        if (success)
+            Debug.Log("Connection established");
+        else
+            Debug.Log("Connection attempt failed or disconnection detected");
+    }
+
+}
