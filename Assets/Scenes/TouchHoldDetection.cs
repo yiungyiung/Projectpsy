f456c5c7ee677f0c8ba2212291b188ac043a5b1a
satvikibudhia <122771370+satvikibudhia@users.noreply.github.com>
2023-08-23 01:14:38 +0530

    touch vs hold

---
+++ Assets/Scenes/TouchHoldDetection.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TouchHoldDetection : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool isBeingHeld = false;
+    private float holdTime = 1.0f; // Adjust this time threshold as needed
+    private float touchStartTime;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isBeingHeld = true;
+        touchStartTime = Time.time;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isBeingHeld = false;
+
+        if (Time.time - touchStartTime < holdTime)
+        {
+            Debug.Log("Touched/Clicked");
+        }
+        else
+        {
+            Debug.Log("Hold");
+        }
+    }
+}
+
---
+++ Assets/Scripts/holdortouch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class holdortouch : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool isBeingHeld = false;
+    private float holdTime = 1.0f;
+    private float touchStartTime;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isBeingHeld = true;
+        touchStartTime = Time.time;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isBeingHeld = false;
+
+        if (Time.time - touchStartTime < holdTime)
+        {
+            Debug.Log("Touched/Clicked");
+        }
+        else
+        {
+            Debug.Log("Hold");
+        }
+    }
+}
