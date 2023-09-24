a1f63e5883a33ebb77b7537b21a57dd4b208c2c9
yiungyiung <yashparkhi.super@gmail.com>
2023-09-24 22:33:10 +0530

    PUSHEDCHANGES

--- Assets/test01.cs
+++ Assets/test01.cs
@@ -6,9 +6,11 @@ public class test01 : MonoBehaviour, IDragHandler, IEndDragHandler
     public Transform pivot; // The central pivot point
     public float maxRadius = 100f; // The maximum allowed radius
     public float returnSpeed = 5f; // Speed at which the button returns to its original position
+    public float smoothTime = 0.3F; // Smoothing time for the dragging movement
 
     private RectTransform rectTransform;
     private Vector2 originalPosition;
+    private Vector2 velocity = Vector2.zero; // This value is modified by the SmoothDamp function each time you call it.
 
     private void Start()
     {
@@ -19,7 +21,12 @@ public class test01 : MonoBehaviour, IDragHandler, IEndDragHandler
     public void OnDrag(PointerEventData eventData)
     {
         Vector2 pivotToCursor = eventData.position - (Vector2)pivot.position;
-        Vector2 clampedPosition = Vector2.ClampMagnitude(pivotToCursor, maxRadius);
+        Vector2 clampedPosition = Vector2.SmoothDamp(rectTransform.anchoredPosition, pivotToCursor, ref velocity, smoothTime);
+
+        if (clampedPosition.magnitude > maxRadius)
+        {
+            clampedPosition = clampedPosition.normalized * maxRadius;
+        }
 
         rectTransform.anchoredPosition = clampedPosition;
     }
@@ -37,10 +44,10 @@ public class test01 : MonoBehaviour, IDragHandler, IEndDragHandler
         while (elapsedTime < 1f)
         {
             elapsedTime += Time.deltaTime * returnSpeed;
-            rectTransform.anchoredPosition = Vector2.Lerp(currentPosition, originalPosition, elapsedTime);
+            rectTransform.anchoredPosition = Vector2.Lerp(currentPosition, originalPosition, Mathf.SmoothStep(0f, 1f, elapsedTime));
             yield return null;
         }
 
         rectTransform.anchoredPosition = originalPosition;
-    }
+}
 }
